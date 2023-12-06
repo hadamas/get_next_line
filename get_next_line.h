79c8e9d458e04979e2dd206efae42c6f9b2ed1a4
Alanis Hadama <ahadama-@student.42.rio>
2023-12-06 19:25:58 -0300

    primeiro commit

---
+++ get_next_line.h
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahadama- <ahadama-@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/06 13:50:54 by ahadama-          #+#    #+#             */
+/*   Updated: 2023/12/06 18:29:48 by ahadama-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+# endif
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+typedef struct s_list
+{
+	char			*str_buffer;
+	struct s_list	*next;
+}	t_list;
+
+int		found_newline(t_list *list);
+t_list	*find_last_node(t_list *list);
+char	*get_line(t_list *list);
+void	copy_str(t_list *list, char *str);
+int		len_to_newline(t_list *list);
+void	polish_list(t_list **list);
+char	*get_next_line(int fd);
+void	dealloc(t_list **list, t_list *clean_node, char *buf);
+void	create_list(t_list **list, int fd);
+
+#endif
